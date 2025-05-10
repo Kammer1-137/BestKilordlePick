95b731bca930ff7a3e8939f48eb9de0d80ee425c
Kammer <162889342+Kammer1-137@users.noreply.github.com>
2025-05-10 19:06:16 +0200

    fixed issue and reached 38 words

--- doubleCoverage.py
+++ doubleCoverage.py
@@ -6,6 +6,23 @@ import wordOperations
 # min_value = min(d.values())
 # min_keys = [k for k in d if d[k] == min_value]
 
+
+def leaveOnlyZeroLetteredWords(words: list[str], usedLettersDF: pd.DataFrame) -> list[str]:
+    acceptedWords = []
+
+    for word in words:
+        addToList = False
+        for pos, letter in enumerate(word):
+            if usedLettersDF.loc[letter, pos] == 0:
+                addToList = True
+                break
+
+        if addToList:
+            acceptedWords.append(word)
+
+    return acceptedWords
+
+
 def doubleCoverage(words: list[str], reservoirDF: pd.DataFrame):
     usedLettersDF = wordOperations.createEmptyDF()
     weightedWords = wordOperations.assignValues(words, reservoirDF, True)
@@ -30,6 +47,9 @@ def doubleCoverage(words: list[str], reservoirDF: pd.DataFrame):
 
     # for _ in range(10):
     while 0 in usedLettersDF.values:
+
+        words = leaveOnlyZeroLetteredWords(words, usedLettersDF)
+
         # print(0 in usedLettersDF.values)
         weightedWords = wordOperations.assignValues(words, usedLettersDF, True)
         min_value = min(weightedWords.values())
@@ -39,10 +59,12 @@ def doubleCoverage(words: list[str], reservoirDF: pd.DataFrame):
 
         weightedWords = wordOperations.assignValues(min_keys, reservoirDF, True)
 
+        # print(weightedWords.keys())
+
         top = weightedWords.popitem()
         topWord, topValue = top[0], top[1]
         usedWords.append(topWord)
-        print(top)
+        print(top, min_value, len(min_keys))
         words.remove(topWord)
 
         for pos, letter in enumerate(topWord):
@@ -51,6 +73,8 @@ def doubleCoverage(words: list[str], reservoirDF: pd.DataFrame):
             # reservoirDF.loc[letter, pos] *= 2
 
         # print(usedLettersDF)
+
+        # print(f'{len(usedWords)=}')
 
     print(usedWords)
 
